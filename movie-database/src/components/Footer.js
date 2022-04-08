1b56f9e2f58a025898d113b67f3cd7728b4b1339
dinimeri <dinimeriawati@gmail.com>
2022-04-08 11:07:35 +0700

    pertemuan-4

---
+++ movie-database/src/components/Footer.js
@@ -0,0 +1,15 @@
+/**
+ * Membuat component Footer
+ * Menampilkan footer web
+ */
+
+const Footer = () => {
+  return(
+    <footer>
+      <h2>Copyright dinimeri</h2>
+      <p>Dibuat menggunakan ReactJS</p>
+    </footer>
+  )
+}
+
+export default Footer;
